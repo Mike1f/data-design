b7980b80fa32862d2ff74138a069bcfcf79553b4
Michael Figueroa <mfigueroa14@cnm.edu>
2018-04-18 09:11:05 -0600

    Data design sql

--- sql/data-design.sql
+++ sql/data-design.sql
@@ -7,12 +7,14 @@ DROP TABLE IF EXISTS article;
 CREATE TABLE category (
 categoryId BINARY (16) NOT NULL,
 categoryName BINARY (16) NOT NULL,
-)
+PRIMARY KEY(categoryId)
+);
 
 CREATE TABLE tag (
 tagId BINARY (16) NOT NULL,
 tagName BINARY (16) NOT NULL,
-)
+PRIMARY KEY (tagId)
+);
 
 CREATE TABLE article (
 articleId BINARY (16) NOT NULL,
@@ -20,4 +22,9 @@ articleName BINARY (16) NOT NULL,
 articleDateTime BINARY (16) NOT NULL,
 articleAuthor BINARY (16) NOT NULL,
 articleContent VARCHAR (240) NOT NULL,
-)
+articleCategoryId BINARY (16) NOT NULL,
+articleTagId BINARY (16) NOT NULL,
+FOREIGN KEY (articleCategoryId),
+FOREIGN KEY (articleTagId),
+PRIMARY KEY (articleId)
+);
